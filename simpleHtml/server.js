77e20d09d3f86b33736a5d4b56012353f14c0c4b
EU FAÇO PROGRAMA <brunopena454@gmail.com>
2025-06-18 21:48:08 -0300

    .
    Tudo funcionando no simpleHTML

--- simpleHtml/server.js
+++ simpleHtml/server.js
@@ -1,15 +1,18 @@
 const nextButton = document.querySelector('.next-btn')
 const video = document.querySelector('.hero-video')
 
-const movieList = ['videos/hero-1.mp4', 'videos/hero-2.mp4', 'videos/hero-3.mp4', 'videos/hero-4.mp4']
+const movieList = [
+  "../mediaFiles/videos/hero-1.mp4",
+  "../mediaFiles/videos/hero-2.mp4",
+  "../mediaFiles/videos/hero-3.mp4",
+  "../mediaFiles/videos/hero-4.mp4"
+]
 
 let index = 0
-nextButton.addEventListener('click', function() {
 
-    index += 1
-    video.src = movieList[index]
-    
-    if (index === 3) {
-        index = -1
-    }
+nextButton.addEventListener('click', () => {
+  index = (index + 1) % movieList.length
+  video.querySelector('source').src = movieList[index]
+  video.load()
+  video.play()
 })
